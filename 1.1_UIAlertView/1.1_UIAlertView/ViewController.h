40d8badecbda74e36a5c79f607abb254d3fd9e9a
Shayne Chow <shayne@authbuy.org>
2014-11-14 13:23:58 +0800

    create project: 1.1_UIAlertView

---
+++ 1.1_UIAlertView/1.1_UIAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  1.1_UIAlertView
+//
+//  Created by choushayne on 14/11/14.
+//  Copyright (c) 2014年 ShayneChow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 1.1_UIAlertView/1.1_UIAlertViewTests/__1_UIAlertViewTests.m
@@ -0,0 +1,40 @@
+//
+//  __1_UIAlertViewTests.m
+//  1.1_UIAlertViewTests
+//
+//  Created by choushayne on 14/11/14.
+//  Copyright (c) 2014年 ShayneChow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface __1_UIAlertViewTests : XCTestCase
+
+@end
+
+@implementation __1_UIAlertViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
