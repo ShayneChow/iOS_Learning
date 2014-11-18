69fe6796538391d47e4bb8cfa711da9c2925cc67
Shayne Chow <shayne@authbuy.org>
2014-11-18 16:16:23 +0800

    制作自定义选择器

--- 2.2UIPickerView/2.2UIPickerView/ViewController.m
+++ 2.2UIPickerView/2.2UIPickerView/ViewController.m
@@ -15,6 +15,11 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
+    
+//    UIPickerView *picker = [[UIPickerView alloc] initWithFrame:CGRectMake(0, 20, 300, 200)];
+//    [self.view addSubview:picker];
+    
+    a=[[NSArray alloc] initWithObjects:@"A",@"B",@"C",@"D",@"E",@"F", nil];
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
@@ -22,6 +27,20 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+//设置自定义选择器显示的栏
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+    return 1;
+}
+//设置自定义选择器每栏显示的行数
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
+    return [a count];
+}
+
+//填充内容
+-(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
+        return [a objectAtIndex:row];
 }
 
 @end
