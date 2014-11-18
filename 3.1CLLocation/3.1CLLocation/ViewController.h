24dfd6b2afc8485c46037b49748a5dbe8d45a0e5
Shayne Chow <shayne@authbuy.org>
2014-11-18 17:40:24 +0800

    创建新项目--定位服务与地图

---
+++ 3.1CLLocation/3.1CLLocation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  3.1CLLocation
+//
+//  Created by choushayne on 14/11/18.
+//  Copyright (c) 2014年 ShayneChow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 3.1CLLocation/3.1CLLocation/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  3.1CLLocation
+//
+//  Created by choushayne on 14/11/18.
+//  Copyright (c) 2014年 ShayneChow. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
