04c6dd974f157940aca3d0ce4cb4b341113da8be
Shayne Chow <shayne@authbuy.org>
2014-11-25 15:26:27 +0800

    加载网页内容
    1、创建 NSURL 对象，初始化网址
    NSURL *url = [NSURL URLWithString:@"http://www.baidu.com/"];
    2、创建 NSURLRequest 对象，传递网址
    NSURLRequest *request = [NSURLRequest requestWithURL:url];
    3、加载网址对应的网页内容
    [_web loadRequest:request];

--- 4.1UIWebView/4.1UIWebView/ViewController.h
+++ 4.1UIWebView/4.1UIWebView/ViewController.h
@@ -10,6 +10,7 @@
 
 @interface ViewController : UIViewController
 
+@property (strong, nonatomic) IBOutlet UIWebView *web;
 
 @end
 
--- 4.1UIWebView/4.1UIWebView/ViewController.m
+++ 4.1UIWebView/4.1UIWebView/ViewController.m
@@ -16,8 +16,15 @@
 
 - (void)viewDidLoad {
     
-    UIWebView *web = [[UIWebView alloc] initWithFrame:CGRectMake(20, 50, 270, 200)];
-    [self.view addSubview:web];
+//    UIWebView *web = [[UIWebView alloc] initWithFrame:CGRectMake(20, 50, 270, 200)];
+//    [self.view addSubview:web];
+    
+    //创建 NSURL 对象，初始化网址
+    NSURL *url = [NSURL URLWithString:@"http://www.baidu.com/"];
+    //创建 NSURLRequest 对象，传递网址
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    //加载网址对应的网页内容
+    [_web loadRequest:request];
     
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
