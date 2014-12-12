3733715d706f15b2b4912770a882a1d763af2b2a
Shayne Chow <shayne@authbuy.org>
2014-12-12 16:13:14 +0800

    创建评分视图demo

---
+++ RatingViewDemo/RatingViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RatingViewDemo
+//
+//  Created by choushayne on 14/12/12.
+//  Copyright (c) 2014年 ShayneChow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
