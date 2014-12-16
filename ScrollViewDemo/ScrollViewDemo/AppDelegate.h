847ed376aa3cf504e308cb4ae85b70a8eaacd03a
Shayne Chow <shayne@authbuy.org>
2014-12-16 17:48:21 +0800

    创建ScrollViewDemo项目

---
+++ ScrollViewDemo/ScrollViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollViewDemo
+//
+//  Created by choushayne on 14/12/16.
+//  Copyright (c) 2014年 ShayneChow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollViewDemo/ScrollViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollViewDemo
+//
+//  Created by choushayne on 14/12/16.
+//  Copyright (c) 2014年 ShayneChow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
