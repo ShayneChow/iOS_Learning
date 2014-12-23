44f080ecb2b983251e2b4f8f58d357599f89353f
Shayne Chow <shayne@authbuy.org>
2014-12-23 21:18:39 +0800

    新特性UIAlertController测试

--- 1.3_UIAlertController/1.3_UIAlertController/ViewController.m
+++ 1.3_UIAlertController/1.3_UIAlertController/ViewController.m
@@ -28,22 +28,32 @@
 
 - (void)buttonForOld{
     UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    UIButton *btn_1 = [UIButton buttonWithType:UIButtonTypeCustom];
     btn.frame = CGRectMake(90,100,140,40);
+    btn_1.frame = CGRectMake(90,200,140,40);
     
     [btn setTitle:@"UIAlertView" forState:UIControlStateNormal];//设置btn标题
+    [btn_1 setTitle:@"UIAlertController" forState:UIControlStateNormal];//设置btn标题
 
 //    [btn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];//设置标题颜色
     
     btn.showsTouchWhenHighlighted = YES;
     
     [btn addTarget:self action:@selector(touchUpInside) forControlEvents:UIControlEventTouchUpInside];
+    [btn_1 addTarget:self action:@selector(touchUpInside_1) forControlEvents:UIControlEventTouchUpInside];
     
     [self.view addSubview:btn];
+    [self.view addSubview:btn_1];
 }
 
 - (void)touchUpInside{
     NSLog(@"touchUpInside");
     [self alertViewInit];
+}
+
+- (void)touchUpInside_1{
+    NSLog(@"touchUpInside");
+    [self alertControllerInit];
 }
 
 - (void)alertViewInit{
@@ -75,5 +85,17 @@
     //显示AlertView
     [alert show];
 }
+
+- (void)alertControllerInit{
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"标题" message:@"这个是UIAlertController的默认样式" preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:nil];
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"好的" style:UIAlertActionStyleDefault handler:nil];
+    [alertController addAction:cancelAction];
+    [alertController addAction:okAction];
+    
+    [self presentViewController:alertController animated:YES completion:nil];
+}
+
 
 @end
