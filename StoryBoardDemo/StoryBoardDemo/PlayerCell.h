b7678dc75b6c010c17f9655112237b4584479a69
Shayne Chow <shayne@authbuy.org>
2014-12-25 10:56:47 +0800

    Using a Subclass for the Cell

---
+++ StoryBoardDemo/StoryBoardDemo/PlayerCell.h
@@ -0,0 +1,17 @@
+//
+//  PlayerCell.h
+//  StoryBoardDemo
+//
+//  Created by choushayne on 14/12/25.
+//  Copyright (c) 2014年 ShayneChow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayerCell : UITableViewCell
+
+@property (nonatomic, weak) IBOutlet UILabel *nameLabel;
+@property (nonatomic, weak) IBOutlet UILabel *gameLabel;
+@property (nonatomic, weak) IBOutlet UIImageView *ratingImageView;
+
+@end
---
+++ StoryBoardDemo/StoryBoardDemo/PlayerCell.m
@@ -0,0 +1,23 @@
+//
+//  PlayerCell.m
+//  StoryBoardDemo
+//
+//  Created by choushayne on 14/12/25.
+//  Copyright (c) 2014年 ShayneChow. All rights reserved.
+//
+
+#import "PlayerCell.h"
+
+@implementation PlayerCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
