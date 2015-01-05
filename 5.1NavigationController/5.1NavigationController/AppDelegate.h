6530bf2fdf81781db0c256c802186ff35c1b98dd
Shayne Chow <shayne@authbuy.org>
2015-01-05 17:26:11 +0800

    创建NavigationController项目

---
+++ 5.1NavigationController/5.1NavigationController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  5.1NavigationController
+//
+//  Created by choushayne on 15/1/5.
+//  Copyright (c) 2015年 ShayneChow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
