3cac0a0218dbe6084d3981d37b85736914ebb97d
Shayne Chow <shayne@authbuy.org>
2015-01-08 17:58:41 +0800

    创建TableView项目

---
+++ 7.1TableViewDemo/7.1TableViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  7.1TableViewDemo
+//
+//  Created by choushayne on 15/1/8.
+//  Copyright (c) 2015年 ShayneChow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 7.1TableViewDemo/7.1TableViewDemoTests/__1TableViewDemoTests.m
@@ -0,0 +1,40 @@
+//
+//  __1TableViewDemoTests.m
+//  7.1TableViewDemoTests
+//
+//  Created by choushayne on 15/1/8.
+//  Copyright (c) 2015年 ShayneChow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface __1TableViewDemoTests : XCTestCase
+
+@end
+
+@implementation __1TableViewDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
