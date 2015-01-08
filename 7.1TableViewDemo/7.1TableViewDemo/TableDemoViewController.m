2258a89dfe9227e36526c8d60c31d63103cdc5cb
Shayne Chow <shayne@authbuy.org>
2015-01-08 18:29:10 +0800

    实现tableview协议，关联IB

---
+++ 7.1TableViewDemo/7.1TableViewDemo/TableDemoViewController.m
@@ -0,0 +1,49 @@
+//
+//  ViewController.m
+//  7.1TableViewDemo
+//
+//  Created by choushayne on 15/1/8.
+//  Copyright (c) 2015年 ShayneChow. All rights reserved.
+//
+
+#import "TableDemoViewController.h"
+
+@interface TableDemoViewController ()
+
+@end
+
+@implementation TableDemoViewController{
+    NSArray *recipes;   // 定义一个处理表格数据的实例变量
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    recipes = [NSArray arrayWithObjects:@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini", nil];  // 初始化数据
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [recipes count];
+}// 通知表格视图在这个区段（section）中有多少行
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"SimpleTableCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    cell.textLabel.text = [recipes objectAtIndex:indexPath.row];
+    return cell;
+}
+
+@end
