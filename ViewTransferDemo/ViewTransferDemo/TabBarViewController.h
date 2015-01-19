a6fd723e64bde3747a2ecec06f26459099c14a87
Shayne Chow <shayne@authbuy.org>
2015-01-19 14:13:47 +0800

    创建TabBarController，使用TabBar控制视图切换

---
+++ ViewTransferDemo/ViewTransferDemo/TabBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  TabBarViewController.h
+//  ViewTransferDemo
+//
+//  Created by choushayne on 15/1/19.
+//  Copyright (c) 2015年 ShayneChow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabBarViewController : UITabBarController
+
+@end
