319a2175d8ea4472f5f28a95b1f6700c5e4f0754
Shayne Chow <shayne@authbuy.org>
2015-02-03 17:40:20 +0800

    创建Parallax视差TableView视图项目

---
+++ ParallaxControllerDemo/ParallaxControllerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ParallaxControllerDemo
+//
+//  Created by choushayne on 15/2/3.
+//  Copyright (c) 2015年 ShayneChow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
